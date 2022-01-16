3eb0b34835cd79932fa0a3095bec964993acc1a1
CDSSOUSA <cleberdossantossousa@gmail.com>
2022-01-16 13:07:42 -0300

    ajustes em link uteis

--- app/Views/site/home/utils.php
+++ app/Views/site/home/utils.php
@@ -1,56 +1,72 @@
-<section class="block-wrapper">
+<section class="block-wrapper p-30 section-bg">
+    namespace App\Views\site\home;
     <div class="container">
         <div class="row">
-
-            <div class="col-lg-2">
-                <div class="ts-grid-box ts-col-box" style="height: 150px;">
-                    <div class="item"><a class="post-cat ts-pink-bg" href="http://www.ibge.gov.br/cidadesat/topwindow.htm?1">Utéis</a>
-                        <div class="post-content">
-                            <h3 class="post-title">
-                                <a href="http://www.ibge.gov.br/cidadesat/topwindow.htm?1">Conheça as Cidades Brasileiras</a>
-                            </h3><span class="post-date-info"><i class="fa fa-clock-o"></i> March 21, 2019</span>
-                        </div>
-                    </div><!-- item end-->
-                </div><!-- ts gird box end-->
-            </div><!-- col end-->
-            <div class="col-lg-2">
-                <div class="ts-grid-box ts-col-box" style="height: 150px;">
-                    <div class="item"><a class="post-cat ts-pink-bg" href="http://www.ibge.gov.br/estadosat">UTÉIS</a>
-                        <div class="post-content">
-                            <h3 class="post-title"><a href="http://www.ibge.gov.br/estadosat">Conheça os Estados Brasileiros</a></h3><span class="post-date-info"><i class="fa fa-clock-o"></i> March 21, 2019</span>
-                        </div>
-                    </div><!-- item end-->
-                </div><!-- ts gird box end-->
-            </div><!-- col end-->
-            <div class="col-lg-2">
-                <div class="ts-grid-box ts-col-box" style="height: 150px;">
-                    <div class="item"><a class="post-cat ts-pink-bg" href="http://www.ibge.gov.br/paisesat">UTÉIS</a>
-                        <div class="post-content">
-                            <h3 class="post-title"><a href="http://www.ibge.gov.br/paisesat">Conheça o Mundo</a></h3><span class="post-date-info"><i class="fa fa-clock-o"></i> March 21, 2019</span>
-                        </div>
-                    </div><!-- item end-->
-                </div><!-- ts gird box end-->
-            </div><!-- col end-->
-            <div class="col-lg-3">
-                <div class="ts-grid-box ts-col-box" style="height: 150px;">
-                    <div class="item"><a class="post-cat ts-pink-bg" href="http://www.tiberiogeo.com.br/artigo/EscravidaoCampinaGrande.pdf">UTÉIS</a>
-                        <div class="post-content">
-                            <h3 class="post-title"><a href="http://www.tiberiogeo.com.br/artigo/EscravidaoCampinaGrande.pdf">HISTÓRIAS DA ESCRAVIDÃO EM CAMPINA GRANDE</a></h3><span class="post-date-info"><i class="fa fa-clock-o"></i> March 21, 2019</span>
-                        </div>
-                    </div><!-- item end-->
-                </div><!-- ts gird box end-->
-            </div><!-- col end-->
-            <div class="col-lg-3">
-                <div class="ts-grid-box ts-col-box" style="height: 150px;">
-                    <div class="item"><a class="post-cat ts-pink-bg" href="http://www.tiberiogeo.com.br/artigo/SombrasEmMovimento.pdf">UTÉIS</a>
-                        <div class="post-content">
-                            <h3 class="post-title"><a href="http://www.tiberiogeo.com.br/artigo/SombrasEmMovimento.pdf">SOMBRAS EM MOVIMENTO:OS ESCRAVOS E O QUEBRA-QUILOS EM CAMPINA GRANDE
-
-</a></h3><span class="post-date-info"><i class="fa fa-clock-o"></i> March 21, 2019</span>
-                        </div>
-                    </div><!-- item end-->
-                </div><!-- ts gird box end-->
-            </div><!-- col end-->
+            <div class="col-lg-12">
+                <div class="ts-title-item clearfix">
+                    <h2 class="ts-cat-title float-left ts-pink-bg"><span>Links Úteis</span></h2>
+                </div>
+                <div class="row ts-col-list-post">
+                    <div class="col-lg-2">
+                        <div class="ts-grid-box ts-col-box" style="height: 150px;">
+                            <div class="item">
+                                <span class="post-cat ts-pink-bg">Interatividade</span>
+                                <div class="post-content">
+                                    <h3 class="post-title">
+                                        <?= anchor('http://www.ibge.gov.br/cidadesat/topwindow.htm?1', 'Conheça as Cidades Brasileiras', ['target' => '_blank']); ?>
+                                    </h3>
+                                </div>
+                            </div><!-- item end-->
+                        </div><!-- ts gird box end-->
+                    </div><!-- col end-->
+                    <div class="col-lg-2">
+                        <div class="ts-grid-box ts-col-box" style="height: 150px;">
+                            <div class="item">
+                                <span class="post-cat ts-pink-bg">Interatividade</span>
+                                <div class="post-content">
+                                    <h3 class="post-title">
+                                        <?= anchor('http://www.ibge.gov.br/estadosat', 'Conheça os Estados Brasileiros', ['target' => '_blank']); ?>
+                                </div>
+                            </div><!-- item end-->
+                        </div><!-- ts gird box end-->
+                    </div><!-- col end-->
+                    <div class="col-lg-2">
+                        <div class="ts-grid-box ts-col-box" style="height: 150px;">
+                            <div class="item">
+                                <span class="post-cat ts-pink-bg">Interatividade</span>
+                                <div class="post-content">
+                                    <h3 class="post-title">
+                                        <?= anchor('http://www.ibge.gov.br/paisesat', 'Conheça o Mundo', ['target' => '_blank']); ?>
+                                </div>
+                            </div><!-- item end-->
+                        </div><!-- ts gird box end-->
+                    </div><!-- col end-->
+                    <div class="col-lg-3">
+                        <div class="ts-grid-box ts-col-box" style="height: 150px;">
+                            <div class="item">
+                                <span class="post-cat ts-pink-bg">Artigo</span>
+                                <div class="post-content">
+                                    <h3 class="post-title">
+                                        <?= anchor('http://www.tiberiogeo.com.br/artigo/EscravidaoCampinaGrande.pdf', 'HISTÓRIAS DA ESCRAVIDÃO EM CAMPINA GRANDE', ['target' => '_blank']); ?>
+                                    </h3>
+                                </div>
+                            </div><!-- item end-->
+                        </div><!-- ts gird box end-->
+                    </div><!-- col end-->
+                    <div class="col-lg-3">
+                        <div class="ts-grid-box ts-col-box" style="height: 150px;">
+                            <div class="item">
+                                <span class="post-cat ts-pink-bg">Artigo</span>
+                                <div class="post-content">
+                                    <h3 class="post-title">
+                                        <?= anchor('http://www.tiberiogeo.com.br/artigo/SombrasEmMovimento.pdf', 'SOMBRAS EM MOVIMENTO:OS ESCRAVOS E O QUEBRA-QUILOS EM CAMPINA GRANDE', ['target' => '_blank']); ?>
+                                    </h3>
+                                </div>
+                            </div><!-- item end-->
+                        </div><!-- ts gird box end-->
+                    </div><!-- col end-->
+                </div>
+            </div>
         </div>
     </div><!-- row end-->
 </section>
